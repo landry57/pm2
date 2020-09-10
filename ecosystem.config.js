a8052c28fd9fb07b9e4e71a73fc712b3ef92201c
landry57 <landry.fof@gmail.com>
2020-09-10 22:15:37 +0000

    pm2 v2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,22 @@
 module.exports = {
-  apps : [{
-    script: 'index.js',
-    watch: '.'
-  }, {
-    script: './service-worker/',
-    watch: ['./service-worker']
-  }],
+    apps: [{
+        script: 'bin/www',
+        watch: '.'
+    }, {
+        script: './service-worker/',
+        watch: ['./service-worker']
+    }],
 
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
+    deploy: {
+        production: {
+            user: 'digitapps',
+            host: '80.241.211.59',
+            ref: 'origin/master',
+            repo: ' https://github.com/landry57/nodedemo.git',
+            path: '/home/digitapps/pm2node',
+            'pre-deploy-local': '',
+            'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
+            'pre-setup': ''
+        }
     }
-  }
 };
